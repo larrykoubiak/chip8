6af4f91082405d52e24486811c6fb06ac2a82681
Marion Denis <I36107@INT.SYS.SHARED.FORTIS>
2017-12-18 16:03:58 +0100

    started implementing opcodes, fixed byte order of opcodes

--- bitfield.py
+++ bitfield.py
@@ -3,20 +3,26 @@ import struct
 c_uint8 = ctypes.c_uint8
 c_uint16 = ctypes.c_uint16
 
+def uint16tobytes(val):
+    return struct.pack('H',val)
+
+def bytestouint16(b):
+    return struct.unpack_from("H",b)[0]
+
 class Opcode4(ctypes.LittleEndianStructure):
     _pack_ = 1
     _fields_ = [
-            ("op2", c_uint8, 4),
-            ("op1", c_uint8, 4),
             ("op4", c_uint8, 4),
             ("op3", c_uint8, 4),
+            ("op2", c_uint8, 4),
+            ("op1", c_uint8, 4),
         ]
 
 class Opcode2(ctypes.LittleEndianStructure):
     _pack_ = 1
     _fields_ = [
-            ("op1", c_uint8, 8),
             ("op2", c_uint8, 8),
+            ("op1", c_uint8, 8),
         ]
 
 class Opcode(ctypes.Union):
--- cpu.py
+++ cpu.py
@@ -1,4 +1,6 @@
 import bitfield
+from bitfield import uint16tobytes, bytestouint16
+import binascii
 
 class CPU:
     def __init__(self):
@@ -7,22 +9,64 @@ class CPU:
         self.stack = bytearray(64)
         self.SP = 0x3F
         self.PC = 0x200
-        self.RAM = bytearray(4096)
-        self.RAM[0x200:0x204] = b'\x3F\x40\xFF\xE0'
+        self.RAM = bytearray(4112)
     def __str__(self):
-        val = "I: %02X\nSP: %02X\nPC: %04X\n" % (self.I, self.SP,self.PC)
+        val = "-" * 25 + "\n"
+        val += "I: %02X\nSP: %02X\nPC: %04X\n" % (self.I, self.SP,self.PC)
         for i in range(0,16):
             val += "V%X: %02X\n" % (i, self.V[i])
+        val += "Stack: " + binascii.hexlify(self.stack[self.SP:0x40]) + "\n"
+        val += "-" * 25
         return val
+
     def read_opcode(self):
         opcode = bitfield.Opcode()
-        opcode.from_bytes(self.RAM[self.PC:self.PC+2])
+        b = self.RAM[self.PC:self.PC+2]
+        opcode.from_bytes(b)
         self.PC += 2
         return opcode
+    def run_opcode(self,opcode):
+        if(opcode.Opcode2.op1 == 0x00):
+            if(opcode.Opcode2.op2 == 0xE0):
+                ## CLS (00E0)
+                print "CLS " + str(opcode)
+                return
+            elif(opcode.Opcode2.op2 == 0xEE):
+            ## RET (00EE)
+                print "RET " + str(opcode)
+                self.PC = bytestouint16(self.stack[self.SP+1:self.SP+3])
+                self.SP += 2
+                return
+        elif(opcode.Opcode4.op1 == 0x1):
+            ## JP (1nnn)
+            print "JP " + str(opcode)
+            addr = opcode.asuint16 & 0xFFF
+            self.PC = addr
+            return
+        elif(opcode.Opcode4.op1 == 0x2):
+            ## CALL (2nnn)
+            print "CALL " + str(opcode)
+            addr = opcode.asuint16 & 0xFFF
+            self.stack[self.SP - 1:self.SP + 1] = uint16tobytes(self.PC)
+            self.SP -= 2
+            self.PC = addr
+            return
+        else:
+            print "NOP"
+            return
+    def print_ram(self):
+        for r in xrange(0,256):
+            print "%03X : " % (r*16) + binascii.hexlify(c.RAM[r*16:(r*16)+16]).upper()
 
 if __name__ == '__main__':
     c = CPU()
+    c.RAM[0x0200:0x0206] = b'\xFE\x1F\xE0\x00\xEE\x2E'
+    c.RAM[0x0EEE:0x0F00] = b'\xEE\x00'
+    c.RAM[0x0FFE:0X1000] = b'\x02\x12'
+    print len(c.RAM)
+    print c
     o = c.read_opcode()
-    print o
-    o = c.read_opcode()
-    print o
+    while o.asuint16 != 0:
+        c.run_opcode(o)
+        print c
+        o = c.read_opcode()
