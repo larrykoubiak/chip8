b5f16ba92b6880098a1206e690ed828af8eaddb1
Marion Denis <denis.marion@bnpparibasfortis.com>
2017-12-27 19:46:59 +0100

    more opcodes, renamed bitfield members to nibbles, bytes and int

--- bitfield.py
+++ bitfield.py
@@ -9,26 +9,26 @@ def uint16tobytes(val):
 def bytestouint16(b):
     return struct.unpack_from("H",b)[0]
 
-class Opcode4(ctypes.LittleEndianStructure):
+class Nibbles(ctypes.LittleEndianStructure):
     _pack_ = 1
     _fields_ = [
-            ("op4", c_uint8, 4),
-            ("op3", c_uint8, 4),
-            ("op2", c_uint8, 4),
-            ("op1", c_uint8, 4),
+            ("n4", c_uint8, 4),
+            ("n3", c_uint8, 4),
+            ("n2", c_uint8, 4),
+            ("n1", c_uint8, 4),
         ]
 
-class Opcode2(ctypes.LittleEndianStructure):
+class Bytes(ctypes.LittleEndianStructure):
     _pack_ = 1
     _fields_ = [
-            ("op2", c_uint8, 8),
-            ("op1", c_uint8, 8),
+            ("b2", c_uint8, 8),
+            ("b1", c_uint8, 8),
         ]
 
 class Opcode(ctypes.Union):
-    _fields_ = [("Opcode4", Opcode4),
-                ("Opcode2", Opcode2),
-                ("asuint16", c_uint16)]
+    _fields_ = [("nib", Nibbles),
+                ("byt", Bytes),
+                ("int", c_uint16)]
     def __init__(self):
         self.asuint16 = 0
     def __str__(self):
@@ -36,7 +36,7 @@ class Opcode(ctypes.Union):
         #fmtOp2 = "Op2_1: %02X Op2_2 : %02X"
     	#result = fmtOp4 % (self.Opcode4.op1, self.Opcode4.op2, self.Opcode4.op3, self.Opcode4.op4)
         #result += " " + fmtOp2 % (self.Opcode2.op1,self.Opcode2.op2) + " Op: %04X" % self.asuint16
-        result = "%04X" % self.asuint16
+        result = "%04X" % self.int
         return result
     def from_bytes(self,b):
-        self.asuint16 = struct.unpack_from("H",b)[0]
+        self.int = struct.unpack_from("H",b)[0]
--- cpu.py
+++ cpu.py
@@ -11,16 +11,14 @@ class CPU:
         self.PC = 0x200
         self.RAM = bytearray(0x1000)
     def __str__(self):
-        val = "-" * 63 + "\n"
+        val =  "+------+--+--++" + "--+" * 16 + "\n"
         val += "|  PC  |I |SP||"
         for i in range(0,16):
             val +="V%X|" % i
-        #val += "\n" + ("-" * 60)
         val += "\n|0x%04X|%02X|%02X||" % (self.PC,self.I, self.SP)
         for i in range(0,16):
             val += "%02X|" % (self.V[i])
-        #val += "\n|Stack|" + binascii.hexlify(self.stack[0:0x1A]) + "|"
-        val += "\n" + ("-" * 63)
+        val += "\n" + "+------+--+--++" + "--+" * 16
         return val
 
     def read_opcode(self):
@@ -30,29 +28,25 @@ class CPU:
             opcode.from_bytes(b)
             self.PC += 2
         return opcode
-    def run_opcode(self,opcode):
-        if(opcode.Opcode2.op1 == 0x00):
-            if(opcode.Opcode2.op2 == 0xE0):
-                ## CLS (00E0)
+    def run_opcode(self,op):
+        if(op.byt.b1 == 0x00):
+            if(op.byt.b2 == 0xE0):
                 print "CLS"
                 return
-            elif(opcode.Opcode2.op2 == 0xEE):
-            ## RET (00EE)
+            elif(op.byt.b2 == 0xEE):
                 print "RET"
                 self.SP -=1
                 self.PC = self.stack[self.SP] << 8
                 self.SP -= 1
                 self.PC += self.stack[self.SP]
                 return
-        elif(opcode.Opcode4.op1 == 0x1):
-            ## JP (1nnn)
-            addr = opcode.asuint16 & 0xFFF
+        elif(op.nib.n1 == 0x1):
+            addr = op.int & 0xFFF
             print "JP 0x%04X" % addr
             self.PC = addr
             return
-        elif(opcode.Opcode4.op1 == 0x2):
-            ## CALL (2nnn)
-            addr = opcode.asuint16 & 0xFFF
+        elif(op.nib.n1 == 0x2):
+            addr = op.int & 0xFFF
             print "CALL 0x%04X" % addr 
             self.stack[self.SP] =self.PC & 0xFF
             self.SP += 1
@@ -60,50 +54,74 @@ class CPU:
             self.SP += 1
             self.PC = addr
             return
-        elif(opcode.Opcode4.op1 == 0x3):
-            ## SE (3xkk)
-            print "SE V%X, 0x%02X" % (opcode.Opcode4.op2,opcode.Opcode2.op2)
-            if self.V[opcode.Opcode4.op2] == opcode.Opcode2.op2:
+        elif(op.nib.n1 == 0x3):
+            print "SE V%X, 0x%02X" % (op.nib.n2,op.byt.b2)
+            if self.V[op.nib.n2] == op.byt.b2:
                 self.PC += 2
             return
-        elif(opcode.Opcode4.op1 == 0x4):
-            ## SNE (4xkk)
-            print "SNE V%X, 0x%02X" % (opcode.Opcode4.op2,opcode.Opcode2.op2)
-            if self.V[opcode.Opcode4.op2] != opcode.Opcode2.op2:
+        elif(op.nib.n1 == 0x4):
+            print "SNE V%X, 0x%02X" % (op.nib.n2,op.byt.b2)
+            if self.V[op.nib.n2] != op.byt.b2:
                 self.PC += 2
             return
-        elif(opcode.Opcode4.op1 == 0x5):
-            ## SE (5xy0)
-            print "SE V%X, V%X" % (opcode.Opcode4.op2,opcode.Opcode4.op3)
-            if self.V[opcode.Opcode4.op2] == self.V[opcode.Opcode4.op3]:
+        elif(op.nib.n1 == 0x5):
+            print "SE V%X, V%X" % (op.nib.n2,op.nib.n3)
+            if self.V[op.nib.n2] == self.V[op.nib.n3]:
                 self.PC += 2
             return
-        elif(opcode.Opcode4.op1 == 0x6):
-            ## LD (6xkk)
-            print "LD V%X, 0x%02X" % (opcode.Opcode4.op2,opcode.Opcode2.op2)
-            self.V[opcode.Opcode4.op2] = opcode.Opcode2.op2
+        elif(op.nib.n1 == 0x6):
+            print "LD V%X, 0x%02X" % (op.nib.n2,op.byt.b2)
+            self.V[op.nib.n2] = op.byt.b2
             return
-        elif(opcode.Opcode4.op1 == 0x7):
-            ## ADD (7xkk)
-            print "ADD V%X, 0x%02X" % (opcode.Opcode4.op2,opcode.Opcode2.op2)
-            val = self.V[opcode.Opcode4.op2] + opcode.Opcode2.op2
-            self.V[opcode.Opcode4.op2] = val & 0xFF
+        elif(op.nib.n1 == 0x7):
+            print "ADD V%X, 0x%02X" % (op.nib.n2,op.byt.b2)
+            val = self.V[op.nib.n2] + op.byt.b2
+            self.V[op.nib.n2] = val & 0xFF
             return
-        elif(opcode.Opcode4.op1 == 0x8):
-            if(opcode.Opcode4.op4 == 0x0):
-                ## LD (8xy0)
-                print "LD V%X, V%X " % (opcode.Opcode4.op2,opcode.Opcode4.op3)
-                self.V[opcode.Opcode4.op2] = self.V[opcode.Opcode4.op3]
+        elif(op.nib.n1 == 0x8):
+            if(op.nib.n4 == 0x0):
+                print "LD V%X, V%X " % (op.nib.n2,op.nib.n3)
+                self.V[op.nib.n2] = self.V[op.nib.n3]
                 return
-            elif(opcode.Opcode4.op4 == 0x1):
-                ## OR (8xy1)
-                print "OR V%X, V%X " % (opcode.Opcode4.op2,opcode.Opcode4.op3)
-                self.V[opcode.Opcode4.op2] |= self.V[opcode.Opcode4.op3]
+            elif(op.nib.n4 == 0x1):
+                print "OR V%X, V%X " % (op.nib.n2,op.nib.n3)
+                self.V[op.nib.n2] |= self.V[op.nib.n3]
                 return
-            elif(opcode.Opcode4.op4 == 0x2):
-                ## AND (8xy2)
-                print "AND V%X, V%X " % (opcode.Opcode4.op2,opcode.Opcode4.op3)
-                self.V[opcode.Opcode4.op2] &= self.V[opcode.Opcode4.op3]
+            elif(op.nib.n4 == 0x2):
+                print "AND V%X, V%X " % (op.nib.n2,op.nib.n3)
+                self.V[op.nib.n2] &= self.V[op.nib.n3]
+                return
+            elif(op.nib.n4 == 0x3):
+                print "XOR V%X, V%X " % (op.nib.n2,op.nib.n3)
+                self.V[op.nib.n2] ^= self.V[op.nib.n3]
+                return
+            elif(op.nib.n4 == 0x4):
+                print "ADD V%X, V%X " % (op.nib.n2,op.nib.n3)
+                val = self.V[op.nib.n2] + self.V[op.nib.n3]
+                self.V[0xF] = val >> 8
+                self.V[op.nib.n2] = val & 0xFF
+                return
+            elif(op.nib.n4 == 0x5):
+                print "SUB V%X, V%X " % (op.nib.n2,op.nib.n3)
+                val = self.V[op.nib.n2] - self.V[op.nib.n3]
+                self.V[0xF] = 1 if val >= 0 else 0
+                self.V[op.nib.n2] = val & 0xFF
+                return
+            elif(op.nib.n4 == 0x6):
+                print "SHR V%X " % (op.nib.n2)
+                self.V[0xF] = self.V[op.nib.n2] & 0x01
+                self.V[op.nib.n2] = self.V[op.nib.n2] >> 1
+                return
+            elif(op.nib.n4 == 0x7):
+                print "SUBN V%X, V%X " % (op.nib.n2,op.nib.n3)
+                val = self.V[op.nib.n3] - self.V[op.nib.n2]
+                self.V[0xF] = 1 if val >= 0 else 0
+                self.V[op.nib.n2] = val & 0xFF
+                return
+            elif(op.nib.n4 == 0xE):
+                print "SHL V%X " % (op.nib.n2)
+                self.V[0xF] = self.V[op.nib.n2] >> 7
+                self.V[op.nib.n2] = self.V[op.nib.n2] << 1
                 return
         else:
             print "NOP"
@@ -117,12 +135,14 @@ if __name__ == '__main__':
     c.RAM[0x0200:0x0206] = b'\xFA\x1F\xE0\x00\xEE\x2E' ## JP FFA - CLS - CALL EEE
     c.RAM[0x0206:0x020C] = b'\x03\x31\xFF\xFF\x03\x41' ## SE 1, 03 - NOP - SNE 1, 03
     c.RAM[0x020C:0x0212] = b'\x00\x82\x11\x82\x02\x82' ## LD 2,0 - OR 2,1 - AND 2,0
+    c.RAM[0x0212:0x0218] = b'\xFE\x63\x13\x83\x14\x83' ## LD 3,FE - XOR 3,1 - ADD 3,1
+    c.RAM[0x0218:0x021E] = b'\x05\x83\x06\x83\x0E\x83' ## LD 3,FE - XOR 3,1 - ADD 3,0    
     c.RAM[0x0EEE:0x0EF4] = b'\x01\x61\x02\x71\xEE\x00' ## LD 1, 01 - ADD 1, 02 - RET
     c.RAM[0x0FFA:0X1000] = b'\x02\x60\x20\x50\x02\x12' ## LD 0, 02 - SE 0,2 - JP 202
     print "START"
     print c
     o = c.read_opcode()
-    while o.asuint16 != 0:
+    while o.int != 0:
         c.run_opcode(o)
         #print c
         o = c.read_opcode()
