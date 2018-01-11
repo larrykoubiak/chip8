07ef44b101601e167905fa73ad59ec61199c786d
Larry Koubiak <koubiak@skynet.be>
2018-01-11 20:28:56 +0100

    added arguments and Gooey GUI for emu options and rom loading

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
 from emu import Emu
+from gooey import Gooey, GooeyParser
 
+@Gooey
+def main():
+    parser = GooeyParser(description='Chip-8 Emulator')
+    parser.add_argument("rom",\
+                        help="ROM file to be executed",\
+                        action="store",\
+                        widget="FileChooser")
+    parser.add_argument("--key_release",\
+                        help="Auto-release keys (needed for some games)",\
+                        action="store_true")
+    parser.add_argument("--debug",\
+                        help="Debug Mode (disassemble rom code)",
+                        action="store_true")
+    parser.add_argument("--scale",\
+                        help="Scale multiplier of window size",
+                        nargs='?',\
+                        default=8,\
+                        type=int)
+    args = parser.parse_args()
+    emu = Emu(args.key_release,args.scale,args.debug)
+    emu.mainloop(args.rom)
 if __name__ == "__main__":
-    emu = Emu(key_release=False,scale=8,debug=False)
-    emu.mainloop('roms/tetris.ch8')
+    main()
